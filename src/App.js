731e827500bc0141688a2bd0bfab2910336d005d
TheBigR <nevoroy@gmail.com>
2021-10-29 14:20:40 +0300

    theme works

--- src/App.js
+++ src/App.js
@@ -3,28 +3,43 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css'
 import { ThemeProvider } from 'styled-components'
 import Header from './components/Header'
+import lightTheme from './theme/light'
+import darkTheme from './theme/dark'
+import Container from './theme/components/Container'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { darkModeAction } from './redux/actions/configActions'
 
 function App() {
+  const config = useSelector((state) => state.config)
+  const themeValue = config.darkMode
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (!config.darkMode) {
+      dispatch(darkModeAction(window.localStorage.getItem('theme')))
+    }
+  }, [config.darkMode, dispatch])
+  console.log(themeValue)
   return (
-    <ThemeProvider className="App ui container">
+    <ThemeProvider theme={themeValue === 'light' ? lightTheme : darkTheme}>
       <BrowserRouter>
-        <div>
-          <Header />
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+        <Header />
+
+        <Container>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React 3
+          </a>
+        </Container>
       </BrowserRouter>
     </ThemeProvider>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import Container from '../theme/components/Container'
+import { darkModeAction } from '../redux/actions/configActions'
 
 const Header = () => {
+  const dispatch = useDispatch()
+
+  const themeChange = (value) => {
+    window.localStorage.setItem('theme', value)
+    dispatch(darkModeAction(value))
+  }
+
   return (
-    <div>
+    <div className="ui green">
       <div className="ui secondary pointing menu">
-        <Link to="/Weather" className="item active">
+        <Link to="/Weather" className="ui button">
           <i className="cloud icon"></i>Weather
         </Link>
-        <Link to="/history" className="item">
+        <Link to="/history" className="ui button">
           <i className="history icon"></i>History
         </Link>
         <div className="right menu">
@@ -16,9 +26,13 @@ const Header = () => {
             <i className="home icon"></i>
           </Link>
           <div className="ui buttons">
-            <button className="ui button">Dark</button>
+            <button className="ui button" onClick={() => themeChange('dark')}>
+              Dark
+            </button>
             <div className="or"></div>
-            <button className="ui button active">Light</button>
+            <button className="ui button" onClick={() => themeChange('light')}>
+              Light
+            </button>
           </div>
         </div>
       </div>
